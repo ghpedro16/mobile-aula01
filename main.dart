cc402c7f215dd2d310864d72f0602062a1e12607
Pedro Henrique <pedrohenrique.phas16@gmail.com>
2025-08-28 00:56:53 +0000

    aula 27/08

--- main.dart
+++ main.dart
@@ -1,3 +1,20 @@
+import 'dart:io';
+
 void main(){
-    print('olá edilson');
+
+    List<String> alunos =[];
+    bool cadastrarOutroAluno = true;
+
+    while(cadastrarOutroAluno){
+    print('Informe o nome do aluno:');
+    String? nome = stdin.readLineSync();
+
+    alunos.add(nome);
+
+    print('Deseja cadastrar outro aluno? (S / N)');
+    var respostaUsuario = stdin.readLineSync();
+    cadastrarOutroAluno = respostaUsuario == "S";
+
+    };
+
 }
